19086e1d20352664dc67200e2a4092488c6557d1
Adrienn Szabo <aszabo@ilab.sztaki.hu>
2016-01-08 15:22:57 +0100

    fixing impl

--- noisifyMatrixUtil.R
+++ noisifyMatrixUtil.R
@@ -1,7 +1,14 @@
 #!/usr/bin/Rscript
 
-generateNoisifiedMatrices <- function(inputMx, num, seedFrom, p, outDir) {
-  print("We'll be writing data into:")
-  print(outDir)
-  # TODO
+generateNoisifiedMatrices <- function(inputMx, num, seedFrom, perturbIntensity, outDir) {
+  # print("We'll be writing data into:")
+  # print(outDir)
+  for (s in seedFrom:(seedFrom + num - 1)) {
+    tmpMx <- Noisify(inputMx, perturbIntensity, as.numeric(s))
+    filename <- paste(outDir, "/rndmx-", s, ".aamx", sep="")
+    myWriter <- file(filename, "w")
+    writeHeaderRows(myWriter, s)
+    write.table(tmpMx, file=filename, quote=FALSE, append = TRUE, col.names = FALSE)
+    close(myWriter)
+  }
 }
